50754c5878f4ad37a7e161c2579a9d010ed23e7e
shubhamdasnadas <sithdalvi123@gmail.com>
2026-01-05 18:35:04 +0530

    data changes

--- TechNMS/src/app/api/sdwan/tunnels/route.ts
+++ TechNMS/src/app/api/sdwan/tunnels/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST() {
   try {
     const base = process.env.VMANAGE_URL;
     const user = process.env.VMANAGE_USER;
