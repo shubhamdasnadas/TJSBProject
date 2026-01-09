14336d6f087c91e69f05ca66bc2132cd9f172e47
D-heeraj7-13 <scholaritian321@gmail.com>
2026-01-09 16:29:09 +0530

    logo half working :v2

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -70,25 +70,7 @@ const Login2 = () => {
                     justifyContent="center"
                     mt={3}
                   >
-                    <Typography
-                      color="textSecondary"
-                      variant="h6"
-                      fontWeight="500"
-                    >
-                      New to Modernize?
-                    </Typography>
-                    <Typography
-                      component={Link}
-                      href="/authentication/register"
-                      fontWeight="500"
-                      sx={{
-                        textDecoration: "none",
-                        color: "primary.main",
-                      }}
-                    >
-                      Create an account
-                    </Typography>
-                  </Stack>
+                   </Stack>
                 }
               />
             </Card>
