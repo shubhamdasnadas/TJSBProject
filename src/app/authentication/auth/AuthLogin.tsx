e8ca647f6fdc66a40c4ddb0603b1e64eb633c802
DHEERAJ <86879344+D-heeraj7-13@users.noreply.github.com>
2026-01-11 10:17:14 +0530

    Enter button working for login hassle

--- src/app/authentication/auth/AuthLogin.tsx
+++ src/app/authentication/auth/AuthLogin.tsx
@@ -10,63 +10,37 @@ import {
   Stack,
   Checkbox,
 } from "@mui/material";
-import Link from "next/link";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 import CustomTextField from "../../(DashboardLayout)/components/forms/theme-elements/CustomTextField";
 
-interface LoginProps {
-  title?: string;
-  subtitle?: React.ReactNode;
-  subtext?: React.ReactNode;
-
-  userData: {
-    userName: string;
-    password: string;
-  };
-
-  setUserData: React.Dispatch<
-    React.SetStateAction<{
-      userName: string;
-      password: string;
-    }>
-  >;
-}
-
-const AuthLogin: React.FC<LoginProps> = ({
+const AuthLogin = ({
   title,
   subtitle,
   subtext,
   userData,
   setUserData,
-}) => {
+}: any) => {
   const router = useRouter();
 
-  const handleSubmit = async () => {
-    console.log("Login submitted:", userData);
+  const handleSubmit = async (e?: React.FormEvent) => {
+    e?.preventDefault(); // 🔥 important for form submit
 
     try {
-      const response = await axios.post(
-        "/api/zabbix-login",
-        {
-          username: userData.userName,
-          password: userData.password,
-        }
-      );
+      const response = await axios.post("/api/zabbix-login", {
+        username: userData.userName,
+        password: userData.password,
+      });
 
       const data = response.data;
 
       if (data.result) {
-        const token = data.result;
-        console.log("User Token:", token);
-
-        localStorage.setItem("zabbix_auth", token);
+        localStorage.setItem("zabbix_auth", data.result);
         localStorage.setItem("zabbix_login_status", "true");
-
         router.push("/");
       } else {
-        console.error("Login Failed:", data.error || "Unknown error");
+        console.error("Login Failed");
       }
     } catch (err) {
       console.error("Login error:", err);
@@ -83,82 +57,56 @@ const AuthLogin: React.FC<LoginProps> = ({
 
       {subtext}
 
-      <Stack>
-        {/* Username */}
-        <Box>
-          <Typography
-            variant="subtitle1"
-            fontWeight={600}
-            component="label"
-            htmlFor="username"
-            mb="5px"
-          >
-            Username
-          </Typography>
-          <CustomTextField
-            id="username"
-            variant="outlined"
-            fullWidth
-            value={userData.userName}
-            onChange={(e:any) =>
-              setUserData({ ...userData, userName: e.target.value })
-            }
-          />
-        </Box>
-
-        {/* Password */}
-        <Box mt="25px">
-          <Typography
-            variant="subtitle1"
-            fontWeight={600}
-            component="label"
-            htmlFor="password"
-            mb="5px"
-          >
-            Password
-          </Typography>
-          <CustomTextField
-            id="password"
-            type="password"
-            variant="outlined"
-            fullWidth
-            value={userData.password}
-            onChange={(e:any) =>
-              setUserData({ ...userData, password: e.target.value })
-            }
-          />
-        </Box>
-
-        {/* Remember + Forgot */}
-        <Stack
-          justifyContent="space-between"
-          direction="row"
-          alignItems="center"
-          my={2}
-        >
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox defaultChecked />}
-              label="Remember this Device"
+      {/* 🔥 FORM START */}
+      <form onSubmit={handleSubmit}>
+        <Stack>
+          {/* Username */}
+          <Box>
+            <Typography fontWeight={600}>Username</Typography>
+            <CustomTextField
+              fullWidth
+              value={userData.userName}
+              onChange={(e: any) =>
+                setUserData({ ...userData, userName: e.target.value })
+              }
             />
-          </FormGroup>
+          </Box>
 
-         </Stack>
-      </Stack>
+          {/* Password */}
+          <Box mt="25px">
+            <Typography fontWeight={600}>Password</Typography>
+            <CustomTextField
+              type="password"
+              fullWidth
+              value={userData.password}
+              onChange={(e: any) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+          </Box>
 
-      {/* Login Button */}
-      <Box>
+          <Stack direction="row" justifyContent="space-between" my={2}>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="Remember this Device"
+              />
+            </FormGroup>
+          </Stack>
+        </Stack>
+
+        {/* 🔥 SUBMIT BUTTON */}
         <Button
-          color="primary"
+          type="submit"      // ⭐ key line
           variant="contained"
           size="large"
           fullWidth
           sx={{ py: 1.6, mb: 2 }}
-          onClick={handleSubmit}
         >
           Sign In
         </Button>
-      </Box>
+      </form>
+      {/* 🔥 FORM END */}
 
       {subtitle}
     </>
